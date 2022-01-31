8ca71c997954ef8bb1b24f7741f480f3ca05e546
Mahendran Sobihan <sobihan.mahedran@epitech.eu>
2022-01-31 10:56:46 +0100

    [ADD] All Commands

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -2,9 +2,13 @@
 
 import sys
 
+GAME_MANAGER = 2
+GAME_PLAYER = 1
+
 class Parser:
     def __init__(self) -> None:
         self.gameBoard = []
+
     def __start(self, value) -> None:
         size = int(value)
         if (size < 5):
@@ -14,15 +18,36 @@ class Parser:
             line = [0] * size
             self.gameBoard.append(line)
         print('OK', flush=True, end='\n')
-    
+
     def __debug(self) -> None:
         print(self.gameBoard)
+
     def __turn(self, position) -> None:
-        pass
+        x, y = list(map(int, position.split(','))) # "3, 2" -> 3, 2
+        self.gameBoard[y][x] = GAME_MANAGER
+        # x, y = ai play(board)..
+        self.gameBoard[y][x] =  GAME_PLAYER
+        self.__print(x, y)
+
     def __begin(self) -> None:
+        # x, y = ai play(board)..
+        # self.gameBoard[y][x] =  GAME_PLAYER
+        #  self.__print(x, y)
         pass
+    def __board(self) -> None:
+        for line in sys.stdin:
+            if (line.rstrip() != "DONE"):
+                x, y, owner = list(map(int, line.split(',')))
+                self.gameBoard[y][x] = owner
+            else:
+                #x, y = ai play(board) ..
+                #self.gameBoard[y][x] = GAME_PLAYER
+                # self.__print(x, y)
+                break
     def __end(self) -> None:
         sys.exit(0)
+    def __print(self, x, y) -> None:
+        print(str(x) + "," + str(y), flush=True, end='\n')
     def __about(self) -> None:
         print("name=\"SLGBrain\", version=\"1.0\", author=\"SLG\", country=\"USA\"", flush=True, end='\n')
     def __parseCmd(self, cmd, args) -> None:
@@ -32,6 +57,8 @@ class Parser:
             self.__turn(args[1])
         elif (cmd == "BEGIN"):
             self.__begin()
+        elif (cmd == "BOARD"):
+            self.__board()
         elif (cmd == "END"):
             self.__end()
         elif (cmd == "ABOUT"):
