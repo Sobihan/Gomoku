ac9972cedce2c0b1da5175ac766261f79cb7aef7
Paul <paul.gallimard@epitech.eu>
2022-02-14 10:43:29 +0100

    [ADD] Find Critical Moves

--- AI/Ai.py
+++ AI/Ai.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python3
 from math import ceil
+from nis import match
 
 GAME_MANAGER = 2
 GAME_PLAYER = 1
 
 class AI:
+    def createPatterns(self):
+        self.winnerPatterns = [
+        [0, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0],
+        [0, 1, 0, 1, 1],
+        [1, 0, 1, 1, 0],
+
+        [1, 1, 1, 0, 1],
+        [1, 1, 0, 1, 1],
+        [1, 0, 1, 1, 1]
+        ]
+
+        self.looserPatterns = [
+        [0, 2, 2, 2, 2],
+        [2, 2, 2, 2, 0],
+        [0, 2, 2, 2, 0]
+        [0, 2, 0, 2, 2],
+        [2, 0, 2, 2, 0],
+        [2, 2, 2, 0, 2],
+        [2, 2, 0, 2, 2],
+        [2, 0, 2, 2, 2],
+        [2, 0, 2, 2]
+        ]
     def findBestMove(self, board):
         cpy = board
         maximum = 0
@@ -54,6 +79,9 @@ class AI:
             board = [newBoard[i:] + r + newBoard[:i] for i, r in enumerate(self.rows(board))]
         return [[c for c in r if c is not None] for r in self.cols(board)]
 
+    def unvertical(self, board):
+        return self.vertical(board)
+
     def getAllCopy(self, board):
         newBoard = self.copy(board)
         verticalBoard = self.vertical(board)
@@ -61,9 +89,130 @@ class AI:
         reverseDiagonal = self.diagonal(board, True)
         return [newBoard, verticalBoard, diagonalBoard, reverseDiagonal]
 
+    def match(board, pattern):
+        for x in range(len(board)):
+            if board[x] == pattern[0] and board[x:x+len(pattern)] == pattern:
+                return True
+        return False
+
+    def getCritcalMove(self, board, who):
+        if who == GAME_PLAYER:
+            for b in board:
+                for i in self.winnerPatterns:
+                    if match(b, i):
+                        return board.index(b)
+        else:
+            for b in board:
+                for i in self.looserPatterns:
+                    if match(b, i):
+                        return board.index(b)
+        if who == GAME_PLAYER:
+            for b in board:
+                for i in self.winnerPatterns:
+                    if match(b, i):
+                        return board.index(b)
+        else:
+            for b in board:
+                for i in self.looserPatterns:
+                    if match(b, i):
+                        return board.index(b)
+        return -1
+    
+    def getValue(board, pattern):
+        for x in range(len(board)):
+            if board[x] == pattern[0] and board[x:x+len(pattern)] == pattern:
+                return x
+        return -1
+    def getPatern(self, board, values, who):
+        if (who == GAME_PLAYER):
+            value = self.getValue(board[values], self.winnerPatterns[0])
+            if value != -1:
+                board[values][value] = 8
+                return board
+            value = self.getValue(board[values], self.winnerPatterns[1])
+            if value != -1:
+                board[values][value + 4] = 8
+                return board
+            value = self.getValue(board[values], self.winnerPatterns[2])
+            if value != -1:
+                board[values][value] = 8
+                return board
+            value = self.getValue(board[values], self.winnerPatterns[3])
+            if (value != -1):
+                board[values][value] = 8
+                return board        
+            value = self.getValue(board[values], self.winnerPatterns[4])
+            if (value != -1):
+                board[values][value + 4] = 8
+                return board
+            value = self.getValue(board[values], self.winnerPatterns[5])
+            if (value != -1):
+                board[values][value + 3] = 8
+                return board
+            value = self.getValue(board[values], self.winnerPatterns[6])
+            if (value != -1):
+                board[values][value + 2] = 8
+                return board
+            value = self.getValue(board[values], self.winnerPatterns[7])
+            if (value != -1):
+                board[values][value + 1] = 8
+                return board
+        else:
+            value = self.getValue(board[values], self.looserPatterns[0])
+            if (value != -1):
+                board[values][value] = 8
+                return board
+            value = self.getValue(board[values], self.looserPatterns[1])
+            if (value != -1):
+                board[values][value + 4] = 8
+                return board
+            value = self.getValue(board[values], self.looserPatterns[2])
+            if (value != -1):
+                board[values][value] = 8
+                return board
+            value = self.getValue(board[values], self.looserPatterns[3])
+            if (value != -1):
+                board[values][value + 2] = 8
+                return board
+            value = self.getValue(board[values], self.looserPatterns[4])
+            if (value != -1):
+                board[values][value + 1] = 8
+                return board
+            value = self.getValue(board[values], self.looserPatterns[5])
+            if (value != -1):
+                board[values][value + 3] = 8
+                return board
+            value = self.getValue(board[values], self.looserPatterns[6])
+            if (value != -1):
+                board[values][value + 2] = 8
+                return board
+            value = self.getValue(board[values], self.looserPatterns[7])
+            if (value != -1):
+                board[values][value + 1] = 8
+                return board
+            value = self.getValue(board[values], self.looserPatterns[8])
+            if (value != -1):
+                board[values][value + 1] = 8
+                return board
+        return board
+    
+    def importantMove(self, who, newBoard, diagonal, reverseDiagonal, verticalBoard):
+        result = self.getCritcalMove(newBoard, who)
+        if result != -1:
+            returnValue = self.getPatern(newBoard, result, who)
+            return self.findBestMove(returnValue)
+        result = self.getCritcalMove(verticalBoard, who)
+        if result != -1:
+            returnValue = self.getPatern(verticalBoard, result, who)
+            returnValue = self.unvertical(verticalBoard)
+            return self.findBestMove(returnValue)
+
+
+
     def play(self, board):
         if self.boardIsEmpty(board):
             middle = ceil(len(board)/2)
             return [middle, middle]
         newBoard, verticalBoard, diagonalBoard, reverseDiagonal = self.getAllCopy(board)
+        self.createPatterns()
         return [0]
